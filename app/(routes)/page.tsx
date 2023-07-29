c0584a0f82fa63981b39aa2044eff3f9f932645d
52code <dlmubupt@gmail.com>
2023-07-29 16:50:03 +0800

    featured products

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,24 @@
+import Container from "@/components/ui/container";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("e1c30750-4867-4178-8221-3b8ba3d8b979");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+}
+
+export default HomePage;
